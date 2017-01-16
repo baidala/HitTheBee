d33daee0d2bad8bd98d94c7932b16dccec98d49b
oracle <stavr@bk.ru>
2017-01-16 13:58:07 +0200

    Initial commit

---
+++ app/src/main/java/ua/itstep/android11/hitthebee/MainActivity.java
@@ -0,0 +1,13 @@
+package ua.itstep.android11.hitthebee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
